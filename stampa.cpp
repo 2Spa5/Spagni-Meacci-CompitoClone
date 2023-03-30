6681b4a4a632004cd4b514a4799e728e153905d6
2Spa5 <spagnimeacci.lorenzo@itismeucci.com>
2023-03-30 13:35:05 +0200

    errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -9,12 +9,13 @@ LORENZO SPAGNI MEACCI 30/3/2023
 using namespace std;
 int main()
 {
-  int i=10 ,n;
+  int i=0, n;
   cout<<"Quante volte voi stampare la frase? ";
   cin>>n;
   while (i<n)
   {
    cout<<"Bada come la fuma"<<endl; 
+   i++
   }
   system("pause");
 }
