dc365e4d48717c81ca9f45efedcedb60d59379b8
2Spa5 <spagnimeacci.lorenzo@itismeucci.com>
2023-03-30 13:27:09 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,8 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+LORENZO SPAGNI MEACCI 30/3/2023
+
 */
 
 # include <iostream>
